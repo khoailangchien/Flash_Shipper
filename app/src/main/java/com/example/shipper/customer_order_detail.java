b18f6c4e74f9391e6d22db3c9d0635a7f07e0527
khoailangchien <khoailangchien@users.noreply.github.com>
2019-03-22 22:27:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/shipper/customer_order_detail.java
@@ -0,0 +1,13 @@
+package com.example.shipper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class customer_order_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_order_detail);
+    }
+}
